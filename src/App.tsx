902b5af02c72528918640c190c24c9dbda5a9345
Anish <xevenbiswas@gmail.com>
2024-07-29 15:15:33 +0530

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,11 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Projects from "./components/Projects";
 import { Testimonials } from "./components/Testimonials";
-// import ThemeTogglebutton from "./components/ui/theme-togggle";
 
 function App() {
   return (
     <>
       <ReactLenis root>
-        {/* <div className="fixed top-2 right-6">
-        <ThemeTogglebutton />
-      </div> */}
         <Navbar />
         <MainHero />
         <Projects />
